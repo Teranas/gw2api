180757681b2d047e5c1675ccb63557ce85c314f6
Teranas <teranas@teranas.de>
2015-09-19 01:29:56 +0200

    Corrected dependency and version.

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   name: 'teranas:gw2api',
   summary: 'A simple Guild Wars 2 API wrapper',
-  version: '1.0.0',
+  version: '1.0.0-beta',
   git: 'https://github.com/Teranas/gw2api'
 });
 
 Package.onUse(function(api) {
+  api.use('underscore', ['client', 'server']);
   api.export('GW2API');
   api.addFiles("GW2API.js", 'server');
 });
