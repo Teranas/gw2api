f498e193de59e96ce52564eafa9932df2236e5d5
Teranas <teranas@teranas.de>
2015-09-19 01:05:30 +0200

    + Added git-property to package.js + Added README.md

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'teranas:gw2api',
   summary: 'A simple Guild Wars 2 API wrapper',
   version: '1.0.0',
-  git: ''
+  git: 'https://github.com/Teranas/gw2api'
 });
 
 Package.onUse(function(api) {
